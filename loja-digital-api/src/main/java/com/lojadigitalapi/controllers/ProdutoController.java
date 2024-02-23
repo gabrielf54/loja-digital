be66908ccf427d0e3ff969f3ec5c24f21b6b3a5d
gabriielf54 <gabriel.nuno22@gmail.com>
2024-02-22 21:32:05 -0300

    change web port

--- loja-digital-api/src/main/java/com/lojadigitalapi/controllers/ProdutoController.java
+++ loja-digital-api/src/main/java/com/lojadigitalapi/controllers/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200") // Substitua pela porta do seu frontend Angular, se diferente
 @RestController
 public class ProdutoController {
     @Autowired
